4a9ea024435d52f4f2dd7440fcc593dfa9b5ebc1
sliveryou <sliveryou@qq.com>
2022-04-27 15:41:35 +0800

    feature(convert): add convert pkg

---
+++ convert/convert.go
@@ -0,0 +1,353 @@
+package convert
+
+import (
+	"encoding/binary"
+	"encoding/hex"
+	"fmt"
+	"math"
+	"strconv"
+	"strings"
+	"unicode/utf8"
+)
+
+// ToString returns the string result converted by src.
+func ToString(src interface{}) string {
+	switch v := src.(type) {
+	case int, int8, int16, int32, int64:
+		return strconv.FormatInt(ToInt64(v), 10)
+	case uint, uint8, uint16, uint32, uint64, uintptr:
+		return strconv.FormatUint(ToUint64(v), 10)
+	case float32, float64, complex64, complex128:
+		return strconv.FormatFloat(ToFloat64(v), 'f', -1, 64)
+	case string:
+		return v
+	case []byte:
+		return string(v)
+	case []rune:
+		return string(v)
+	case bool:
+		return strconv.FormatBool(v)
+	case nil:
+		return ""
+	default:
+		return fmt.Sprint(v)
+	}
+}
+
+// ToBool returns the bool result converted by src.
+func ToBool(src interface{}) bool {
+	switch v := src.(type) {
+	case int, int8, int16, int32, int64:
+		return ToInt64(v) > 0
+	case uint, uint8, uint16, uint32, uint64, uintptr:
+		return ToUint64(v) > 0
+	case float32, float64, complex64, complex128:
+		return ToFloat64(v) > 0
+	case bool:
+		return v
+	case string, []byte, []rune:
+		result, _ := strconv.ParseBool(ToString(v))
+		return result
+	default:
+		return false
+	}
+}
+
+// ToInt returns the int result converted by src.
+func ToInt(src interface{}) int {
+	return int(ToInt64(src))
+}
+
+// ToInt32 returns the int32 result converted by src.
+func ToInt32(src interface{}) int32 {
+	return int32(ToInt64(src))
+}
+
+// ToInt64 returns the int64 result converted by src.
+func ToInt64(src interface{}) int64 {
+	switch v := src.(type) {
+	case int:
+		return int64(v)
+	case int8:
+		return int64(v)
+	case int16:
+		return int64(v)
+	case int32:
+		return int64(v)
+	case int64:
+		return v
+	case uint:
+		return int64(v)
+	case uint8:
+		return int64(v)
+	case uint16:
+		return int64(v)
+	case uint32:
+		return int64(v)
+	case uint64:
+		return int64(v)
+	case uintptr:
+		return int64(v)
+	case float32:
+		return int64(v)
+	case float64:
+		return int64(v)
+	case complex64:
+		return int64(real(v))
+	case complex128:
+		return int64(real(v))
+	case bool:
+		if v {
+			return 1
+		}
+		return 0
+	case string:
+		v = strings.TrimSpace(v)
+		index := strings.Index(v, ".")
+		if index != -1 {
+			v = v[:index]
+		}
+		result, _ := strconv.ParseInt(v, 10, 64)
+		return result
+	case []byte:
+		return BytesToInt64(v)
+	default:
+		return 0
+	}
+}
+
+// ToUint returns the uint result converted by src.
+func ToUint(src interface{}) uint {
+	return uint(ToUint64(src))
+}
+
+// ToUint32 returns the uint32 result converted by src.
+func ToUint32(src interface{}) uint32 {
+	return uint32(ToUint64(src))
+}
+
+// ToUint64 returns the uint64 result converted by src.
+func ToUint64(src interface{}) uint64 {
+	switch v := src.(type) {
+	case int:
+		return uint64(v)
+	case int8:
+		return uint64(v)
+	case int16:
+		return uint64(v)
+	case int32:
+		return uint64(v)
+	case int64:
+		return uint64(v)
+	case uint:
+		return uint64(v)
+	case uint8:
+		return uint64(v)
+	case uint16:
+		return uint64(v)
+	case uint32:
+		return uint64(v)
+	case uint64:
+		return v
+	case uintptr:
+		return uint64(v)
+	case float32:
+		return uint64(v)
+	case float64:
+		return uint64(v)
+	case complex64:
+		return uint64(real(v))
+	case complex128:
+		return uint64(real(v))
+	case bool:
+		if v {
+			return 1
+		}
+		return 0
+	case string:
+		v = strings.TrimSpace(v)
+		index := strings.Index(v, ".")
+		if index != -1 {
+			v = v[:index]
+		}
+		result, _ := strconv.ParseUint(v, 10, 64)
+		return result
+	case []byte:
+		return BytesToUint64(v)
+	default:
+		return 0
+	}
+}
+
+// ToFloat returns the float64 result converted by src.
+func ToFloat(src interface{}) float64 {
+	return ToFloat64(src)
+}
+
+// ToFloat32 returns the float32 result converted by src.
+func ToFloat32(src interface{}) float32 {
+	return float32(ToFloat64(src))
+}
+
+// ToFloat64 returns the float64 result converted by src.
+func ToFloat64(src interface{}) float64 {
+	switch v := src.(type) {
+	case int, int8, int16, int32, int64:
+		return float64(ToInt64(v))
+	case uint, uint8, uint16, uint32, uint64, uintptr:
+		return float64(ToUint64(v))
+	case float32:
+		return float64(v)
+	case float64:
+		return v
+	case complex64:
+		return float64(real(v))
+	case complex128:
+		return real(v)
+	case bool:
+		if v {
+			return 1
+		}
+		return 0
+	case string:
+		v = strings.TrimSpace(v)
+		result, _ := strconv.ParseFloat(v, 64)
+		return result
+	case []byte:
+		return BytesToFloat64(v)
+	default:
+		return 0
+	}
+}
+
+// BytesToInt64 returns the int64 result converted by byte slice bytes.
+func BytesToInt64(bytes []byte) int64 {
+	return int64(BytesToUint64(bytes))
+}
+
+// Int64ToBytes returns the byte slice result converted by int64 i.
+func Int64ToBytes(i int64) []byte {
+	return Uint64ToBytes(uint64(i))
+}
+
+// BytesToUint64 returns the uint64 result converted by byte slice bytes.
+func BytesToUint64(bytes []byte) uint64 {
+	return binary.BigEndian.Uint64(bytes)
+}
+
+// Uint64ToBytes returns the byte slice result converted by uint64 i.
+func Uint64ToBytes(i uint64) []byte {
+	bytes := make([]byte, 8)
+	binary.BigEndian.PutUint64(bytes, i)
+	return bytes
+}
+
+// BytesToFloat64 returns the float64 result converted by byte slice bytes.
+func BytesToFloat64(bytes []byte) float64 {
+	bits := binary.BigEndian.Uint64(bytes)
+	return math.Float64frombits(bits)
+}
+
+// Float64ToBytes returns the byte slice result converted by float64 f.
+func Float64ToBytes(f float64) []byte {
+	bits := math.Float64bits(f)
+	bytes := make([]byte, 8)
+	binary.BigEndian.PutUint64(bytes, bits)
+	return bytes
+}
+
+// BytesToRunes returns the rune slice result converted by byte slice bytes.
+func BytesToRunes(bytes []byte) []rune {
+	size, count := utf8.RuneCount(bytes), 0
+	runes := make([]rune, size)
+	for i := 0; i < size; i++ {
+		r, c := utf8.DecodeRune(bytes[count:])
+		runes[i], count = r, count+c
+	}
+	return runes
+}
+
+// RunesToBytes returns the byte slice result converted by rune slice runes.
+func RunesToBytes(runes []rune) []byte {
+	size := 0
+	for _, r := range runes {
+		size += utf8.RuneLen(r)
+	}
+	bytes := make([]byte, size)
+	count := 0
+	for _, r := range runes {
+		count += utf8.EncodeRune(bytes[count:], r)
+	}
+	return bytes
+}
+
+// BytesEncodeHex returns the hexadecimal encoding string result of byte slice bytes.
+func BytesEncodeHex(bytes []byte) string {
+	return hex.EncodeToString(bytes)
+}
+
+// HexDecodeBytes returns the byte slice result represented by the hexadecimal string h.
+func HexDecodeBytes(h string) []byte {
+	bytes, _ := hex.DecodeString(h)
+	return bytes
+}
+
+// BytesEncodeHexs returns the hexadecimal encoding byte slice result of byte slice bytes.
+func BytesEncodeHexs(bytes []byte) []byte {
+	hexs := make([]byte, hex.EncodedLen(len(bytes)))
+	n := hex.Encode(hexs, bytes)
+	return hexs[:n]
+}
+
+// HexsDecodeBytes returns the byte slice result represented by the hexadecimal byte slice hs.
+func HexsDecodeBytes(hs []byte) []byte {
+	bytes := make([]byte, hex.DecodedLen(len(hs)))
+	n, err := hex.Decode(bytes, hs)
+	if err != nil {
+		return nil
+	}
+	return bytes[:n]
+}
+
+// ToBase returns the toBase string result converted by fromBase string src.
+func ToBase(src string, fromBase, toBase int) string {
+	i, err := strconv.ParseInt(src, fromBase, 64)
+	if err != nil {
+		return ""
+	}
+	return strconv.FormatInt(i, toBase)
+}
+
+// DecToBin returns the binary string result converted by decimal int64 dec.
+func DecToBin(dec int64) string {
+	return strconv.FormatInt(dec, 2)
+}
+
+// BinToDec returns the decimal int64 result converted by binary string bin.
+func BinToDec(bin string) int64 {
+	dec, _ := strconv.ParseInt(strings.TrimPrefix(bin, "0b"), 2, 64)
+	return dec
+}
+
+// HexToBin returns the binary string result converted by hexadecimal string hex.
+func HexToBin(hex string) string {
+	i, _ := strconv.ParseInt(strings.TrimPrefix(hex, "0x"), 16, 64)
+	return strconv.FormatInt(i, 2)
+}
+
+// BinToHex returns the hexadecimal string result converted by binary string bin.
+func BinToHex(bin string) string {
+	i, _ := strconv.ParseInt(strings.TrimPrefix(bin, "0b"), 2, 64)
+	return strconv.FormatInt(i, 16)
+}
+
+// DecToHex returns the hexadecimal string result converted by decimal int64 dec.
+func DecToHex(dec int64) string {
+	return strconv.FormatInt(dec, 16)
+}
+
+// HexToDec returns the decimal int64 result converted by hexadecimal string hex.
+func HexToDec(hex string) int64 {
+	dec, _ := strconv.ParseInt(strings.TrimPrefix(hex, "0x"), 16, 64)
+	return dec
+}
---
+++ convert/convert_test.go
@@ -0,0 +1,580 @@
+package convert
+
+import (
+	"math"
+	"strconv"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestToString(t *testing.T) {
+	cases := []struct {
+		src    interface{}
+		expect string
+	}{
+		{src: 123456, expect: "123456"},
+		{src: int64(13579), expect: "13579"},
+		{src: -123456, expect: "-123456"},
+		{src: int64(-13579), expect: "-13579"},
+		{src: uint(123456), expect: "123456"},
+		{src: uint64(13579), expect: "13579"},
+		{src: 1234.5678, expect: "1234.5678"},
+		{src: float32(1234.5), expect: "1234.5"},
+		{src: 12345.12345 + 6666i, expect: "12345.12345"},
+		{src: complex64(1234.5 + 6666i), expect: "1234.5"},
+		{src: -1234.5678, expect: "-1234.5678"},
+		{src: float32(-1234.5), expect: "-1234.5"},
+		{src: -12345.12345 + 6666i, expect: "-12345.12345"},
+		{src: complex64(-1234.5 + 6666i), expect: "-1234.5"},
+		{src: true, expect: "true"},
+		{src: false, expect: "false"},
+		{src: "to string", expect: "to string"},
+		{src: []byte("to string"), expect: "to string"},
+		{src: []rune("to string"), expect: "to string"},
+		{src: []string{"a", "b", "c", "d"}, expect: "[a b c d]"},
+		{src: nil, expect: ""},
+	}
+
+	for _, c := range cases {
+		get := ToString(c.src)
+		assert.Equal(t, c.expect, get)
+	}
+}
+
+func TestToBool(t *testing.T) {
+	cases := []struct {
+		src    interface{}
+		expect bool
+	}{
+		{src: 123456, expect: true},
+		{src: int64(13579), expect: true},
+		{src: -123456, expect: false},
+		{src: int64(-13579), expect: false},
+		{src: uint(123456), expect: true},
+		{src: uint64(13579), expect: true},
+		{src: 1234.5678, expect: true},
+		{src: float32(1234.5), expect: true},
+		{src: 12345.12345 + 6666i, expect: true},
+		{src: complex64(1234.5 + 6666i), expect: true},
+		{src: -1234.5678, expect: false},
+		{src: float32(-1234.5), expect: false},
+		{src: -12345.12345 + 6666i, expect: false},
+		{src: complex64(-1234.5 + 6666i), expect: false},
+		{src: true, expect: true},
+		{src: false, expect: false},
+		{src: "TOO BOOL", expect: false},
+		{src: "TRUE", expect: true},
+		{src: "false", expect: false},
+		{src: []byte("true"), expect: true},
+		{src: []byte("to string"), expect: false},
+		{src: []rune("to string"), expect: false},
+		{src: []string{"a", "b", "c", "d"}, expect: false},
+		{src: nil, expect: false},
+	}
+
+	for _, c := range cases {
+		get := ToBool(c.src)
+		assert.Equal(t, c.expect, get)
+	}
+}
+
+func TestToInt(t *testing.T) {
+	cases := []struct {
+		src    interface{}
+		expect int
+	}{
+		{src: 123456, expect: 123456},
+		{src: int64(13579), expect: 13579},
+		{src: -123456, expect: -123456},
+		{src: int64(-13579), expect: -13579},
+		{src: uint(123456), expect: 123456},
+		{src: uint64(13579), expect: 13579},
+		{src: uint64(1<<64 - 1), expect: -1}, // overflow
+		{src: 1234.5678, expect: 1234},
+		{src: float32(1234.5), expect: 1234},
+		{src: 12345.12345 + 6666i, expect: 12345},
+		{src: complex64(1234.5 + 6666i), expect: 1234},
+		{src: -1234.5678, expect: -1234},
+		{src: float32(-1234.5), expect: -1234},
+		{src: -12345.12345 + 6666i, expect: -12345},
+		{src: complex64(-1234.5 + 6666i), expect: -1234},
+		{src: true, expect: 1},
+		{src: false, expect: 0},
+		{src: "to string", expect: 0},
+		{src: "1234", expect: 1234},
+		{src: "1234.5678", expect: 1234},
+		{src: "  123456 ", expect: 123456},
+		{src: "  1234.5678 ", expect: 1234},
+		{src: Int64ToBytes(12345678), expect: 12345678},
+		{src: []rune("to string"), expect: 0},
+		{src: []string{"a", "b", "c", "d"}, expect: 0},
+		{src: nil, expect: 0},
+	}
+
+	for _, c := range cases {
+		get := ToInt(c.src)
+		assert.Equal(t, c.expect, get)
+	}
+}
+
+func TestToInt32(t *testing.T) {
+	cases := []struct {
+		src    interface{}
+		expect int32
+	}{
+		{src: 123456, expect: 123456},
+		{src: int64(13579), expect: 13579},
+		{src: -123456, expect: -123456},
+		{src: int64(-13579), expect: -13579},
+		{src: uint(123456), expect: 123456},
+		{src: uint64(13579), expect: 13579},
+		{src: uint64(1<<64 - 1), expect: -1}, // overflow
+		{src: 1234.5678, expect: 1234},
+		{src: float32(1234.5), expect: 1234},
+		{src: 12345.12345 + 6666i, expect: 12345},
+		{src: complex64(1234.5 + 6666i), expect: 1234},
+		{src: -1234.5678, expect: -1234},
+		{src: float32(-1234.5), expect: -1234},
+		{src: -12345.12345 + 6666i, expect: -12345},
+		{src: complex64(-1234.5 + 6666i), expect: -1234},
+		{src: true, expect: 1},
+		{src: false, expect: 0},
+		{src: "to string", expect: 0},
+		{src: "1234", expect: 1234},
+		{src: "1234.5678", expect: 1234},
+		{src: "  123456 ", expect: 123456},
+		{src: "  1234.5678 ", expect: 1234},
+		{src: Int64ToBytes(12345678), expect: 12345678},
+		{src: []rune("to string"), expect: 0},
+		{src: []string{"a", "b", "c", "d"}, expect: 0},
+		{src: nil, expect: 0},
+	}
+
+	for _, c := range cases {
+		get := ToInt32(c.src)
+		assert.Equal(t, c.expect, get)
+	}
+}
+
+func TestToInt64(t *testing.T) {
+	cases := []struct {
+		src    interface{}
+		expect int64
+	}{
+		{src: 123456, expect: 123456},
+		{src: int64(13579), expect: 13579},
+		{src: -123456, expect: -123456},
+		{src: int64(-13579), expect: -13579},
+		{src: uint(123456), expect: 123456},
+		{src: uint64(13579), expect: 13579},
+		{src: uint64(1<<64 - 1), expect: -1}, // overflow
+		{src: 1234.5678, expect: 1234},
+		{src: float32(1234.5), expect: 1234},
+		{src: 12345.12345 + 6666i, expect: 12345},
+		{src: complex64(1234.5 + 6666i), expect: 1234},
+		{src: -1234.5678, expect: -1234},
+		{src: float32(-1234.5), expect: -1234},
+		{src: -12345.12345 + 6666i, expect: -12345},
+		{src: complex64(-1234.5 + 6666i), expect: -1234},
+		{src: true, expect: 1},
+		{src: false, expect: 0},
+		{src: "to string", expect: 0},
+		{src: "1234", expect: 1234},
+		{src: "1234.5678", expect: 1234},
+		{src: "  123456 ", expect: 123456},
+		{src: "  1234.5678 ", expect: 1234},
+		{src: Int64ToBytes(12345678), expect: 12345678},
+		{src: []rune("to string"), expect: 0},
+		{src: []string{"a", "b", "c", "d"}, expect: 0},
+		{src: nil, expect: 0},
+	}
+
+	for _, c := range cases {
+		get := ToInt64(c.src)
+		assert.Equal(t, c.expect, get)
+	}
+}
+
+func TestToUint(t *testing.T) {
+	cases := []struct {
+		src    interface{}
+		expect uint
+	}{
+		{src: 123456, expect: 123456},
+		{src: int64(13579), expect: 13579},
+		{src: uint(123456), expect: 123456},
+		{src: uint64(13579), expect: 13579},
+		{src: 1234.5678, expect: 1234},
+		{src: float32(1234.5), expect: 1234},
+		{src: 12345.12345 + 6666i, expect: 12345},
+		{src: complex64(1234.5 + 6666i), expect: 1234},
+		{src: true, expect: 1},
+		{src: false, expect: 0},
+		{src: "to string", expect: 0},
+		{src: "1234", expect: 1234},
+		{src: "1234.5678", expect: 1234},
+		{src: "  123456 ", expect: 123456},
+		{src: "  1234.5678 ", expect: 1234},
+		{src: Uint64ToBytes(12345678), expect: 12345678},
+		{src: []rune("to string"), expect: 0},
+		{src: []string{"a", "b", "c", "d"}, expect: 0},
+		{src: nil, expect: 0},
+	}
+
+	for _, c := range cases {
+		get := ToUint(c.src)
+		assert.Equal(t, c.expect, get)
+	}
+}
+
+func TestToUint32(t *testing.T) {
+	cases := []struct {
+		src    interface{}
+		expect uint32
+	}{
+		{src: 123456, expect: 123456},
+		{src: int64(13579), expect: 13579},
+		{src: -123456, expect: uint32(1<<32-1) - 123456 + 1},      // reverse
+		{src: int64(-13579), expect: uint32(1<<32-1) - 13579 + 1}, // reverse
+		{src: uint(123456), expect: 123456},
+		{src: uint64(13579), expect: 13579},
+		{src: 1234.5678, expect: 1234},
+		{src: float32(1234.5), expect: 1234},
+		{src: 12345.12345 + 6666i, expect: 12345},
+		{src: complex64(1234.5 + 6666i), expect: 1234},
+		{src: -1234.5678, expect: uint32(1<<32-1) - 1234 + 1},                 // reverse
+		{src: float32(-1234.5), expect: uint32(1<<32-1) - 1234 + 1},           // reverse
+		{src: -12345.12345 + 6666i, expect: uint32(1<<32-1) - 12345 + 1},      // reverse
+		{src: complex64(-1234.5 + 6666i), expect: uint32(1<<32-1) - 1234 + 1}, // reverse
+		{src: true, expect: 1},
+		{src: false, expect: 0},
+		{src: "to string", expect: 0},
+		{src: "1234", expect: 1234},
+		{src: "1234.5678", expect: 1234},
+		{src: "  123456 ", expect: 123456},
+		{src: "  1234.5678 ", expect: 1234},
+		{src: Uint64ToBytes(12345678), expect: 12345678},
+		{src: []rune("to string"), expect: 0},
+		{src: []string{"a", "b", "c", "d"}, expect: 0},
+		{src: nil, expect: 0},
+	}
+
+	for _, c := range cases {
+		get := ToUint32(c.src)
+		assert.Equal(t, c.expect, get)
+	}
+}
+
+func TestToUint64(t *testing.T) {
+	cases := []struct {
+		src    interface{}
+		expect uint64
+	}{
+		{src: 123456, expect: 123456},
+		{src: int64(13579), expect: 13579},
+		{src: -123456, expect: uint64(1<<64-1) - 123456 + 1},      // reverse
+		{src: int64(-13579), expect: uint64(1<<64-1) - 13579 + 1}, // reverse
+		{src: uint(123456), expect: 123456},
+		{src: uint64(13579), expect: 13579},
+		{src: uint64(1<<64 - 1), expect: uint64(1<<64 - 1)},
+		{src: 1234.5678, expect: 1234},
+		{src: float32(1234.5), expect: 1234},
+		{src: 12345.12345 + 6666i, expect: 12345},
+		{src: complex64(1234.5 + 6666i), expect: 1234},
+		{src: -1234.5678, expect: uint64(1<<64-1) - 1234 + 1},                 // reverse
+		{src: float32(-1234.5), expect: uint64(1<<64-1) - 1234 + 1},           // reverse
+		{src: -12345.12345 + 6666i, expect: uint64(1<<64-1) - 12345 + 1},      // reverse
+		{src: complex64(-1234.5 + 6666i), expect: uint64(1<<64-1) - 1234 + 1}, // reverse
+		{src: true, expect: 1},
+		{src: false, expect: 0},
+		{src: "to string", expect: 0},
+		{src: "1234", expect: 1234},
+		{src: "1234.5678", expect: 1234},
+		{src: "  123456 ", expect: 123456},
+		{src: "  1234.5678 ", expect: 1234},
+		{src: Uint64ToBytes(12345678), expect: 12345678},
+		{src: []rune("to string"), expect: 0},
+		{src: []string{"a", "b", "c", "d"}, expect: 0},
+		{src: nil, expect: 0},
+	}
+
+	for _, c := range cases {
+		get := ToUint64(c.src)
+		assert.Equal(t, c.expect, get)
+	}
+}
+
+func TestToFloat(t *testing.T) {
+	cases := []struct {
+		src    interface{}
+		expect float64
+	}{
+		{src: 123456, expect: 123456},
+		{src: int64(13579), expect: 13579},
+		{src: -123456, expect: -123456},
+		{src: int64(-13579), expect: -13579},
+		{src: uint(123456), expect: 123456},
+		{src: uint64(13579), expect: 13579},
+		{src: 1234.5678, expect: 1234.5678},
+		{src: float32(1234.5), expect: 1234.5},
+		{src: 12345.12345 + 6666i, expect: 12345.12345},
+		{src: complex64(1234.5 + 6666i), expect: 1234.5},
+		{src: -1234.5678, expect: -1234.5678},
+		{src: float32(-1234.5), expect: -1234.5},
+		{src: -12345.12345 + 6666i, expect: -12345.12345},
+		{src: complex64(-1234.5 + 6666i), expect: -1234.5},
+		{src: true, expect: 1},
+		{src: false, expect: 0},
+		{src: "to string", expect: 0},
+		{src: "1234", expect: 1234},
+		{src: "1234.5678", expect: 1234.5678},
+		{src: "  123456 ", expect: 123456},
+		{src: "  1234.5678 ", expect: 1234.5678},
+		{src: Float64ToBytes(123.456), expect: 123.456},
+		{src: []rune("to string"), expect: 0},
+		{src: []string{"a", "b", "c", "d"}, expect: 0},
+		{src: nil, expect: 0},
+	}
+
+	for _, c := range cases {
+		get := ToFloat(c.src)
+		assert.Equal(t, c.expect, get)
+	}
+}
+
+func TestToFloat32(t *testing.T) {
+	cases := []struct {
+		src    interface{}
+		expect float32
+	}{
+		{src: 123456, expect: 123456},
+		{src: int64(13579), expect: 13579},
+		{src: -123456, expect: -123456},
+		{src: int64(-13579), expect: -13579},
+		{src: uint(123456), expect: 123456},
+		{src: uint64(13579), expect: 13579},
+		{src: 1234.5678, expect: 1234.5678},
+		{src: float32(1234.5), expect: 1234.5},
+		{src: 12345.12345 + 6666i, expect: 12345.12345},
+		{src: complex64(1234.5 + 6666i), expect: 1234.5},
+		{src: -1234.5678, expect: -1234.5678},
+		{src: float32(-1234.5), expect: -1234.5},
+		{src: -12345.12345 + 6666i, expect: -12345.12345},
+		{src: complex64(-1234.5 + 6666i), expect: -1234.5},
+		{src: true, expect: 1},
+		{src: false, expect: 0},
+		{src: "to string", expect: 0},
+		{src: "1234", expect: 1234},
+		{src: "1234.5678", expect: 1234.5678},
+		{src: "  123456 ", expect: 123456},
+		{src: "  1234.5678 ", expect: 1234.5678},
+		{src: Float64ToBytes(123.456), expect: 123.456},
+		{src: []rune("to string"), expect: 0},
+		{src: []string{"a", "b", "c", "d"}, expect: 0},
+		{src: nil, expect: 0},
+	}
+
+	for _, c := range cases {
+		get := ToFloat32(c.src)
+		assert.Equal(t, c.expect, get)
+	}
+}
+
+func TestToFloat64(t *testing.T) {
+	cases := []struct {
+		src    interface{}
+		expect float64
+	}{
+		{src: 123456, expect: 123456},
+		{src: int64(13579), expect: 13579},
+		{src: -123456, expect: -123456},
+		{src: int64(-13579), expect: -13579},
+		{src: uint(123456), expect: 123456},
+		{src: uint64(13579), expect: 13579},
+		{src: 1234.5678, expect: 1234.5678},
+		{src: float32(1234.5), expect: 1234.5},
+		{src: 12345.12345 + 6666i, expect: 12345.12345},
+		{src: complex64(1234.5 + 6666i), expect: 1234.5},
+		{src: -1234.5678, expect: -1234.5678},
+		{src: float32(-1234.5), expect: -1234.5},
+		{src: -12345.12345 + 6666i, expect: -12345.12345},
+		{src: complex64(-1234.5 + 6666i), expect: -1234.5},
+		{src: true, expect: 1},
+		{src: false, expect: 0},
+		{src: "to string", expect: 0},
+		{src: "1234", expect: 1234},
+		{src: "1234.5678", expect: 1234.5678},
+		{src: "  123456 ", expect: 123456},
+		{src: "  1234.5678 ", expect: 1234.5678},
+		{src: Float64ToBytes(123.456), expect: 123.456},
+		{src: []rune("to string"), expect: 0},
+		{src: []string{"a", "b", "c", "d"}, expect: 0},
+		{src: nil, expect: 0},
+	}
+
+	for _, c := range cases {
+		get := ToFloat64(c.src)
+		assert.Equal(t, c.expect, get)
+	}
+}
+
+func TestInt64BytesConversion(t *testing.T) {
+	cases := []struct {
+		src int64
+	}{
+		{src: 1<<63 - 1},
+		{src: -1 << 63},
+		{src: 1<<31 - 1},
+		{src: -1 << 31},
+		{src: 0},
+		{src: 1},
+	}
+
+	for _, c := range cases {
+		get := Int64ToBytes(c.src)
+		assert.Equal(t, c.src, BytesToInt64(get))
+		t.Log(get)
+	}
+}
+
+func TestUint64BytesConversion(t *testing.T) {
+	cases := []struct {
+		src uint64
+	}{
+		{src: 1<<64 - 1},
+		{src: 1<<32 - 1},
+		{src: 1<<16 - 1},
+		{src: 1<<8 - 1},
+		{src: 0},
+		{src: 1},
+	}
+
+	for _, c := range cases {
+		get := Uint64ToBytes(c.src)
+		assert.Equal(t, c.src, BytesToUint64(get))
+		t.Log(get)
+	}
+}
+
+func TestFloat64BytesConversion(t *testing.T) {
+	cases := []struct {
+		src float64
+	}{
+		{src: math.MaxFloat32},
+		{src: math.SmallestNonzeroFloat32},
+		{src: math.MaxFloat64},
+		{src: math.SmallestNonzeroFloat64},
+		{src: 123.123454678090000},
+		{src: 0},
+		{src: 1},
+	}
+
+	for _, c := range cases {
+		get := Float64ToBytes(c.src)
+		assert.Equal(t, c.src, BytesToFloat64(get))
+		t.Log(get)
+	}
+}
+
+func TestRunesBytesConversion(t *testing.T) {
+	cases := []struct {
+		src string
+	}{
+		{src: "Hello，世界。"},
+		{src: "ABCDEFGH"},
+		{src: "0123456789"},
+		{src: "abcdefgh"},
+		{src: "0123456789abcdefghABCDEFGH"},
+		{src: ""},
+	}
+
+	for _, c := range cases {
+		b1 := []byte(c.src)
+		r1 := []rune(c.src)
+		b2 := RunesToBytes(r1)
+		r2 := BytesToRunes(b1)
+		assert.Equal(t, b1, b2)
+		assert.Equal(t, r1, r2)
+		assert.Equal(t, c.src, string(r1))
+		assert.Equal(t, c.src, string(r2))
+		t.Log(b2, r2)
+	}
+}
+
+func TestHexBytesConversion(t *testing.T) {
+	cases := []struct {
+		src string
+	}{
+		{src: "Hello，世界。"},
+		{src: "ABCDEFGH"},
+		{src: "0123456789"},
+		{src: "abcdefgh"},
+		{src: "0123456789abcdefghABCDEFGH"},
+		{src: ""},
+	}
+
+	for _, c := range cases {
+		b1 := []byte(c.src)
+		hex := BytesEncodeHex(b1)
+		b2 := HexDecodeBytes(hex)
+		assert.Equal(t, b1, b2)
+		t.Log(hex)
+	}
+}
+
+func TestHexsBytesConversion(t *testing.T) {
+	cases := []struct {
+		src string
+	}{
+		{src: "Hello，世界。"},
+		{src: "ABCDEFGH"},
+		{src: "0123456789"},
+		{src: "abcdefgh"},
+		{src: "0123456789abcdefghABCDEFGH"},
+		{src: ""},
+	}
+
+	for _, c := range cases {
+		b1 := []byte(c.src)
+		hexs := BytesEncodeHexs(b1)
+		b2 := HexsDecodeBytes(hexs)
+		assert.Equal(t, b1, b2)
+		t.Log(hexs)
+	}
+}
+
+func TestBaseConversion(t *testing.T) {
+	cases := []struct {
+		src int64
+	}{
+		{src: 1<<63 - 1},
+		{src: -1 << 63},
+		{src: 1<<31 - 1},
+		{src: -1 << 31},
+		{src: 0},
+		{src: 1},
+	}
+
+	for _, c := range cases {
+		// decimal conversion
+		d2b := DecToBin(c.src)
+		d2h := DecToHex(c.src)
+		// binary conversion
+		b2d := BinToDec(d2b)
+		b2h := BinToHex(d2b)
+		assert.Equal(t, c.src, b2d)
+		assert.Equal(t, d2h, b2h)
+		// hexadecimal conversion
+		h2d := HexToDec(d2h)
+		h2b := HexToBin(d2h)
+		assert.Equal(t, c.src, h2d)
+		assert.Equal(t, d2b, h2b)
+		// base conversion
+		srcString := strconv.FormatInt(c.src, 10)
+		f10t2 := ToBase(srcString, 10, 2)
+		f2t16 := ToBase(f10t2, 2, 16)
+		f16t10 := ToBase(f2t16, 16, 10)
+		assert.Equal(t, f16t10, srcString)
+	}
+}
